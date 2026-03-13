b0d92d4f7fdcbbdd20cdae78516c480be5a29d8d
DragonFive <maxwell@hacari.com>
2026-03-13 11:13:31 +0800

    feat(rec): add OneRec foundation and runtime wiring without atb submodule dependency.

--- xllm/core/distributed_runtime/rec_engine.cpp
+++ xllm/core/distributed_runtime/rec_engine.cpp
@@ -537,14 +537,10 @@ bool RecEngine::OneRecEnginePipeline::init_model_workers(
     engine_.process_groups_.clear();
     engine_.process_groups_.emplace_back(
         std::make_unique<ProcessGroup>(/*rank=*/0, world_size, devices[0]));
-  }
-#if defined(USE_NPU)
-  else {
+  } else if (devices[0].is_privateuseone()) {
     engine_.process_groups_ =
         parallel_state::create_npu_process_groups(devices);
-  }
-#else
-  else {
+  } else {
     engine_.process_groups_ =
         parallel_state::create_local_process_groups(devices, engine_.options_);
   }
@@ -552,12 +548,13 @@ bool RecEngine::OneRecEnginePipeline::init_model_workers(
 
   engine_.workers_.clear();
   WorkerType worker_type = WorkerType::REC;
-  for (int32_t rank = 0; rank < world_size; ++rank) {
-    ProcessGroup* pg = engine_.process_groups_[rank].get();
+  for (size_t i = 0; i < devices.size(); ++i) {
+    const int32_t rank = static_cast<int32_t>(i);
+    ProcessGroup* pg = engine_.process_groups_[i].get();
     ParallelArgs parallel_args(rank, world_size, pg);
     parallel_args.tp_group_ = pg;
     engine_.workers_.emplace_back(std::make_unique<Worker>(
-        parallel_args, devices[rank], engine_.options_, worker_type));
+        parallel_args, devices[i], engine_.options_, worker_type));
   }
 
   std::vector<folly::SemiFuture<bool>> futures;
